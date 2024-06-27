5adb8ca92c4d985cea93ddbd16b93dffef404b63
MrSeager <seager1993@gmail.com>
2024-06-27 11:26:36 +0300

    Update RecipePage.js

--- src/RecipePage.js
+++ src/RecipePage.js
@@ -14,12 +14,11 @@ import ImgDish from './assets/images/image-omelette.jpeg';
 const RecipePage = () => {
     AOS.init({
         duration: 700,
-        delay: 400,
-        anchorPlacement: 'top-center'
+        delay: 300,
     });
 
     return (
-        <Container className='py-lg-5 py-0'>
+        <Container className='py-lg-5 pt-0 pb-5'>
             <Container className='bg-white d-flex flex-column align-items-start gap-4 p-4 my-0 my-lg-4 rounded cs-w'>
                 <Image data-aos="fade-down" fluid src={ImgDish} alt='dish' className='rounded w-100' />
                 <h1 data-aos="fade-up" className='h1 mt-3 cs-f cs-fw-4 cs-fc'>Simple Omelette Recipe</h1>
